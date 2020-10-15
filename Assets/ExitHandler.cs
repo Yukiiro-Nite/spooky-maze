4cccc2977870addabda9190d1e7160e1cd119355
Yukiiro-Nite <e.kazuma@gmail.com>
2020-10-15 19:28:42 -0400

    Added ladder interaction sound and delay before next level.

--- Assets/ExitHandler.cs
+++ Assets/ExitHandler.cs
@@ -6,8 +6,11 @@ using Valve.VR.InteractionSystem;
 
 public class ExitHandler : MonoBehaviour {
   private MapManager mapManager;
+  private AudioSource audio;
+  private bool exiting = false;
   void Start() {
     mapManager = GameObject.Find("MapManager").GetComponent<MapManager>();
+    audio = GetComponent<AudioSource>();
   }
 
   private void OnHandHoverBegin(Hand hand) {
@@ -21,9 +24,22 @@ public class ExitHandler : MonoBehaviour {
   private void HandHoverUpdate(Hand hand) {
     GrabTypes grabType = hand.GetGrabStarting();
 
-    if(grabType != GrabTypes.None) {
+    if(grabType != GrabTypes.None && !exiting) {
       Debug.Log("Creating new maze");
-      mapManager.InitializeMaze("cave");
+      DoExit();
     }
+  }
+
+  private void DoExit() {
+    exiting = true;
+    audio.Play();
+    StartCoroutine(Wait(3.0f));
+  }
+ 
+  private IEnumerator Wait(float seconds) {
+      yield return new WaitForSeconds(seconds);
+      mapManager.InitializeMaze("cave");
+
+      exiting = false;
   }
 }
